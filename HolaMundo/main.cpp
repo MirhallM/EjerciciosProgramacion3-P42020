bcb4218c613c805ca6b4cd2c54fa3c1c2ee8d0c9
Marcelo M <molinamarcelo113@gmail.com>
2020-10-14 10:11:28 -0600

    Add project files.

---
+++ HolaMundo/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <conio.h>
+
+int main() {
+
+	//Console output 
+	/*Este es un comentario
+	* en varias
+	* lineas */
+
+	std::cout << "Bienvenidos a Programacion 3!\n" << std::endl;
+
+	_getch();
+	return 0;
+}
